ecebca62406cf3f0f8ef4cb22a6d3454821fbef9
j0zko <jozefgunar952@gmail.com>
2025-02-09 14:06:57 +0100

    adding __init__ initialization off objects

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 class Pes:
+
+  def __init__(self, stekot,chodenie,hranie_sa):
+    self.stekot = stekot
+    self.chodenie = chodenie
+    self.hranie_sa = hranie_sa
+
+    def stekot(self):
+      return self.stekot
+    
+    def chodenie(self):
+      return self.chodenie
   
-  def stekot(self):
-    print("Stekam")
+    def hranie_sa(self):
+      return self.hranie_sa
     
 p = Pes()
 print(p.stekot())
-#output: Stekam    
+#output: Stekam
+print(type(p))
+#output: <class main.Pes>
+    
